45b3573252333b17c52b65dbe79c60eae61834db
chhabinath <chhabinath1519@gmail.com>
2024-03-21 00:09:40 +0530

    Learned so much about Animations with so many examples

--- app/src/main/java/com/chhabinath/animations/MainActivity.java
+++ app/src/main/java/com/chhabinath/animations/MainActivity.java
@@ -3,12 +3,66 @@ package com.chhabinath.animations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtAnim;
+
+    Button btnTranslate;
+    Button btnAlpha;
+    Button btnRotate;
+    Button btnScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtAnim = findViewById(R.id.textAnim);
+
+        btnTranslate = findViewById(R.id.btnTranslate);
+        btnAlpha = findViewById(R.id.btnAlpha);
+        btnRotate = findViewById(R.id.btnRotate);
+        btnScale = findViewById(R.id.btnScale);
+
+
+
+        btnTranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation move = AnimationUtils.loadAnimation(MainActivity.this,R.anim.move);
+                txtAnim.startAnimation(move);
+            }
+        });
+
+        btnAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation alpha = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+                txtAnim.startAnimation(alpha);
+            }
+        });
+
+        btnRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+                txtAnim.startAnimation(rotate);
+            }
+        });
+
+        btnScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation scale = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+                txtAnim.startAnimation(scale);
+            }
+        });
+
     }
 }
